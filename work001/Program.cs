6477af71a18c9dd6ca8553caa867cc27981a6df8
MarkusLinV <maksim.lin.00@mail.ru>
2023-10-02 11:09:49 +0200

    все готово

---
+++ work001/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int GetNumber(string message)
+{
+    Console.Write($"Введите число {message}: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void Nto1(int nam)
+{
+    if(nam == 0)
+    {
+        return;
+    }
+    Console.Write($"{nam}, ");
+    nam = nam -1;
+    Nto1(nam);
+}
+
+int nam = GetNumber("");
+
+Nto1(nam);
+
---
+++ work002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetNumber(string message)
+{
+    Console.Write($"Введите число {message}: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int SumFromMtoN(int M, int N)
+{
+    int Sum = 0;
+    for (int i = M; i <= N; i++)
+    {
+        Sum = Sum + i;
+    }
+    return Sum;
+}
+
+int M = GetNumber("M");
+int N = GetNumber("N");
+
+int Sum = SumFromMtoN(M,N);
+Console.WriteLine(Sum);
+
+
